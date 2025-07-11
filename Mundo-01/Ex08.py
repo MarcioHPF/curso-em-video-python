739f9be6c65f1f72739a7915a859f57ceb93226f
MarcioHPF <marcio.mhpf@gmail.com>
2025-07-11 20:33:47 +0000

    Adicionei mais medidas

--- Mundo-01/Ex08.py
+++ Mundo-01/Ex08.py
@@ -1,2 +1,3 @@
 num = float(input('Digite um valor em metros: '))
-print(f'{num:.2f}m é o equivalente a {num * 100:.2f}cm e {num * 1000:.2f}mm')
+print(f'{num}m é o equivalente a: \n{num * 10:.3f}dm  \n{num * 100:.3f}cm \n{num * 1000:.3f}mm \n{num / 10:.3f}dam')
+print(f'{num / 100:.3f}hm \n{num / 1000:.3f}km')
