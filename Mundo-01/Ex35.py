419168706dcb65434b708d017256a8fb420696a8
MarcioHPF <marcio.mhpf@gmail.com>
2025-07-21 20:13:19 +0000

    Corrigi o ex35

--- Mundo-01/Ex35.py
+++ Mundo-01/Ex35.py
@@ -2,5 +2,7 @@ ladoA = float(input('Digite o valor da reta A: '))
 ladoB = float(input('Digite o valor da reta B: '))
 ladoC = float(input('Digite o valor da reta C: '))
 
-if ladoA + laboB > ladoC and ladoB + ladoC > ladoA and ladoA + ladoC > ladoB:
+if ladoA + ladoB > ladoC and ladoB + ladoC > ladoA and ladoA + ladoC > ladoB:
     print('As três retas podem formar um triângulo!')
+else: 
+    print('As três retas não podem formar um triângulo!')
