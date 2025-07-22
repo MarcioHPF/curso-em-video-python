4aeda6dec25d0a14315d39945ee6aa4534b24092
MarcioHPF <marcio.mhpf@gmail.com>
2025-07-22 18:12:42 +0000

    Fiz o ex38

---
+++ Mundo-02/Ex38.py
@@ -0,0 +1,10 @@
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+
+if a > b:
+    print('O primeiro número é maior.')
+elif a < b:
+    print('O segundo número é maior')
+else:
+    print('Os números são iguais')
+    
