bf9336973be21b6bb47e632fd365223c0892a3b1
MarcioHPF <marcio.mhpf@gmail.com>
2025-08-07 19:13:24 +0000

    Fiz o ex95

--- Mundo-03/Ex95.py
+++ Mundo-03/Ex95.py
@@ -1,28 +1,44 @@
-lista = {}
-cont = total = 0
-gols = []
+lista_geral = []
+cont_jogador = 0
 
-lista['Nome'] = str(input('Nome do jogador: ')).strip().capitalize()
-jogos = int(input(f'Quantos jogos o {lista["Nome"]} jogou? '))
+while True:
+    jogador = {} 
+    gols = []
 
-while cont < jogos:
-    gols.append(int(input(f'Quantos gols no {cont + 1}º jogo ? ')))
-    cont+= 1
+    cont_jogador += 1
+    jogador['Num'] = cont_jogador
+    jogador['Nome'] = str(input('Nome do jogador: ')).strip().capitalize()
+    jogos = int(input(f'Quantos jogos o {jogador["Nome"]} jogou? '))
 
-lista['Gols'] = gols
+    for i in range(0, jogos):
+        gols.append(int(input(f'Quantos gols no {i + 1}º jogo? ')))
 
-lista['Total'] = sum(gols)
-print('-' * 45)
-print(lista)
+    jogador['Gols'] = gols
+    jogador['Total'] = sum(gols)
+    lista_geral.append(jogador)
 
-print('-' * 45)
-for chave, valor in lista.items():
-    print(f'O campo {chave} tem o valor {valor}')
+    opc = ' '
+    while opc not in 'SN':
+        opc = str(input('Deseja continuar [S/N]? ')).strip().upper()[0]
+    if opc == 'N':
+        break
 
-print('-' * 45)
+print('-' * 50)
+print(f'{"Nº":<4}{"Jogador":<15}{"Gols":<25}{"Total":<16}')
+print('-' * 50)
 
-print(f'O jogador {lista["Nome"]} jogou {jogos} jogos.')
-for c in range(0,len(gols)):
-    print(f'Na partida {c+1}, fez {gols[c]} gols.')
+for jogador in lista_geral:
+    print(f'{jogador["Num"]:<4}{jogador["Nome"]:<15}{str(jogador["Gols"]):<25}{jogador["Total"]:<16}')
 
-print(f'Totalizando {lista["Total"]} gols.')
+while True:
+    checa = int(input('Deseja ver o resultado de qual jogador?[999 para sair] '))
+    if checa == 999:
+        print('Programa encerrado!')
+        break
+    else:
+        if checa > len(lista_geral):
+            print('Não existe jogador com esse código!')
+        else:
+            print(f'Dados do jogador {lista_geral[checa - 1]['Nome']}:')
+            for indice, gols in enumerate(lista_geral[checa - 1]['Gols']):
+                print(f'    Na partida {indice + 1}, fez {gols} gols;')
